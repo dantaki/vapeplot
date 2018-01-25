ae20ae5cbe72d41fa333f42b115bd180aa33238a
Danny Antaki <dantaki@ucsd.edu>
2018-01-25 11:25:46 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # vapeplot
-matplotlib extension for vaporwave aesthetics
+
+---
+
+matplotlib extension for vaporwave aesthetics 
+
+---
+
+## install
+
+```
+pip install 
+```
+
+## demo
+
+```
+import vapeplot
+import matplotlib.pyplot as plt
+%matplotlib inline 
+
+# simple vaporwave palette
+pal = vapeplot.palettes().vaporwave
+for x in range(0,len(pal)):
+	plt.scatter(x,1,color=pal[x],s=250)
+plt.show()
+```
+
+## api
---
+++ vapeplot.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+class palettes():
+    def __init__(self):
+        self.vaporwave = ['#94D0FF','#8795E8','#966bff','#AD8CFF','#C774E8','#c774a9','#FF6AD5','#ff6a8b','#ff8b8b','#ffa58b','#ffde8b','#cdde8b','#8bde8b','#20de8b'] 
+def despine(ax):
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.get_xaxis().tick_bottom()
+    ax.get_yaxis().tick_left()
+def font_size(s):
+    import matplotlib
+    matplotlib.rcParams.update({'font.size': s})
